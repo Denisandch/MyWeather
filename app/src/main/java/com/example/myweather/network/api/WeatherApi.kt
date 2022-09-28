40e4513ec2ada0b237f88dafac907e5b6b5f8011
Denis Samolovov <denissamolovov@gmail.com>
2022-09-28 16:53:26 +0300

    -added lint to website with API (in constans file) -added imagebuttons

--- app/src/main/java/com/example/myweather/network/api/WeatherApi.kt
+++ app/src/main/java/com/example/myweather/network/api/WeatherApi.kt
@@ -8,10 +8,11 @@ import retrofit2.http.Query
 
 interface WeatherApi {
     @GET("v1/forecast.json")
-    suspend fun getWeekWeather(@Query("key") key: String = API_KEY,
-                                @Query("q") city: String,
-                                @Query("days") days: String = DAYS_COUNT,
-                                @Query("aqi") aqi: String = "no",
-                                @Query("alerts") alerts: String = "no"
+    suspend fun getWeekWeather(
+        @Query("key") key: String = API_KEY,
+        @Query("q") city: String,
+        @Query("days") days: String = DAYS_COUNT,
+        @Query("aqi") aqi: String = "no",
+        @Query("alerts") alerts: String = "no"
     ): JsonAnswer
 }
--- app/src/main/java/com/example/myweather/utils/Constans.kt
+++ app/src/main/java/com/example/myweather/utils/Constans.kt
@@ -1,6 +1,7 @@
 package com.example.myweather.utils
 
 object Constans {
+    //  https://weatherapi.com/
     const val API_KEY = "c3f11a91efbd4bceaf6125403221309"
     const val BASE_URL = "https://api.weatherapi.com/"
     const val DAYS_COUNT = "7"
